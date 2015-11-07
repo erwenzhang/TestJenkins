7d4fb9bcc0e97717ca4b760a2da7ea1dbc4c0caa
erwenzhang <zhangvivien@126.com>
2015-11-07 10:17:44 -0600

    add test 6

--- tests/ExperimentTest.py
+++ tests/ExperimentTest.py
@@ -24,7 +24,9 @@ class MyTestCase(TestCase):
     r1 = Experiment.largest([3,-2,3,-1,4]) 
     self.assertEqual(r1, 4)
 
-
+  def test_t6(self):
+      r1 = Experiment.largest([-3,-2,-3,-1,-4]) 
+      self.assertEqual(r1, -1)
 
   #def test_t7(self):
   #  self.assertRaises(ValueError, Experiment.largest, [])
